fe96dccd132e41b6dfcead663aed9d7eb31dd11c
Jeff <jeff.neumann@phoenix.edu>
2017-07-22 13:22:53 -0400

    Font-Awesome added and start and layout views created. CH1 complete.

--- src/Chapter1/gulpfile.js
+++ src/Chapter1/gulpfile.js
@@ -4,10 +4,10 @@ Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 */
 
 var gulp = require('gulp');
-var gulp = require('gulp-sass');
+var gulpSass = require('gulp-sass');
 
 gulp.task('compile-sass', function () {
     // that space was included in text, but not sure if needed
     gulp.src('./wwwroot/lib/bootstrap/scss/bootstrap.scss')
-        .pipe(gulpSass()) .pipe(gulp.dest('./wwwroot/css'));
+        .pipe(gulpSass()).pipe(gulp.dest('./wwwroot/css'));
 });
